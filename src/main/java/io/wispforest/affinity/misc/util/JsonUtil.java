452389b2e1a88ac6a5a1d48a3d7be75998678010
glisco <glisco03@web.de>
2022-04-15 22:44:58 +0200

    add readNbt and readIngredientList to JsonUtil

--- src/main/java/io/wispforest/affinity/misc/util/JsonUtil.java
+++ src/main/java/io/wispforest/affinity/misc/util/JsonUtil.java
@@ -1,12 +1,20 @@
 package io.wispforest.affinity.misc.util;
 
 import com.google.gson.JsonObject;
+import com.google.gson.JsonParseException;
 import com.google.gson.JsonSyntaxException;
 import com.mojang.brigadier.StringReader;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NbtCompound;
 import net.minecraft.nbt.StringNbtReader;
+import net.minecraft.recipe.Ingredient;
+import net.minecraft.util.Identifier;
 import net.minecraft.util.JsonHelper;
+import net.minecraft.util.registry.Registry;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class JsonUtil {
 
@@ -27,19 +35,37 @@ public class JsonUtil {
 
         final var stack = new ItemStack(item, count);
 
-        final var nbtJson = JsonHelper.getObject(stackObject, "data", null);
-        if (nbtJson != null) {
-            final var nbtString = nbtJson.getAsString();
-            final var reader = new StringNbtReader(new StringReader(nbtString));
-
-            try {
-                stack.setNbt(reader.parseCompound());
-            } catch (CommandSyntaxException e) {
-                throw new JsonSyntaxException("Invalid NBT data found for item '" + item + "' with json '" + stackObject.getAsString() + "'", e);
-            }
+        if (stackObject.has("data")) {
+            stack.setNbt(readNbt(json, "data"));
         }
 
         return stack;
+    }
+
+    public static <T> T readFromRegistry(JsonObject json, String key, Registry<T> registry) {
+        return registry.get(new Identifier(JsonHelper.getString(json, key)));
+    }
+
+    public static NbtCompound readNbt(JsonObject json, String key) {
+        final var nbtString = JsonHelper.getObject(json, key).toString();
+        final var reader = new StringNbtReader(new StringReader(nbtString));
+
+        try {
+            return reader.parseCompound();
+        } catch (CommandSyntaxException e) {
+            throw new JsonParseException("Invalid NBT data found: '" + nbtString + "'", e);
+        }
+    }
+
+    public static List<Ingredient> readIngredientList(JsonObject json, String key) {
+        final var array = JsonHelper.getArray(json, key);
+        final var list = new ArrayList<Ingredient>();
+
+        for (var element : array) {
+            list.add(Ingredient.fromJson(element));
+        }
+
+        return list;
     }
 
 }
