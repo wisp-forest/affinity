69f0a15fba75849f2dcd492f4d1a5b522327026f
Basique Evangelist <basiqueevangelist@yandex.ru>
2022-06-17 00:51:39 +0300

    epic changes

---
+++ src/main/java/io/wispforest/affinity/mixin/WorldMixin.java
@@ -0,0 +1,32 @@
+package io.wispforest.affinity.mixin;
+
+import io.wispforest.affinity.component.AffinityComponents;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraft.world.chunk.EmptyChunk;
+import net.minecraft.world.chunk.WorldChunk;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(World.class)
+public abstract class WorldMixin {
+    @Shadow public abstract WorldChunk getWorldChunk(BlockPos pos);
+
+    @Redirect(method = "hasRain", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;isRaining()Z"))
+    private boolean useLocalWeather(World instance, BlockPos pos) {
+        if (instance.isClient)
+            return instance.isRaining();
+
+        var chunk = getWorldChunk(pos);
+
+        if (chunk instanceof EmptyChunk) {
+            return instance.isRaining();
+        }
+
+        var component = AffinityComponents.LOCAL_WEATHER.get(chunk);
+
+        return component.getRainGradient() > 0.2;
+    }
+}
