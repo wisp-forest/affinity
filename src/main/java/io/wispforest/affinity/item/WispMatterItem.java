340ebafea191a89d107cc0ec7fe16710c97df035
glisco <glisco03@web.de>
2022-10-05 00:21:25 +0200

    remove wisp matter tree finding debug function

--- src/main/java/io/wispforest/affinity/item/WispMatterItem.java
+++ src/main/java/io/wispforest/affinity/item/WispMatterItem.java
@@ -1,19 +1,14 @@
 package io.wispforest.affinity.item;
 
 import io.wispforest.affinity.Affinity;
-import io.wispforest.affinity.misc.util.BlockFinder;
-import io.wispforest.affinity.object.AffinityBlocks;
 import io.wispforest.affinity.object.AffinityItems;
 import io.wispforest.affinity.object.AffinityParticleSystems;
 import io.wispforest.affinity.object.wisps.WispType;
 import io.wispforest.owo.ops.TextOps;
-import net.minecraft.block.Blocks;
-import net.minecraft.block.LeavesBlock;
 import net.minecraft.client.item.TooltipContext;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUsageContext;
-import net.minecraft.tag.BlockTags;
 import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
@@ -44,35 +39,7 @@ public class WispMatterItem extends Item {
             return ActionResult.SUCCESS;
         }
 
-        if (!world.getBlockState(context.getBlockPos()).isIn(BlockTags.LOGS)) return ActionResult.PASS;
-        if (world.isClient) return ActionResult.SUCCESS;
-
-        final var results = BlockFinder.findCapped(world, context.getBlockPos(), (blockPos, state) -> {
-            if (state.isIn(BlockTags.LEAVES)) {
-                return !state.get(LeavesBlock.PERSISTENT);
-            }
-
-            return state.isIn(BlockTags.LOGS);
-        }, 128);
-        final var counted = results.byCount();
-
-        final var player = context.getPlayer();
-        final int logCount = counted.getOrDefault(AffinityBlocks.AZALEA_LOG, 0);
-        final int leavesCount = counted.getOrDefault(Blocks.AZALEA_LEAVES, 0) + counted.getOrDefault(Blocks.FLOWERING_AZALEA_LEAVES, 0);
-
-        if (logCount > 5 && leavesCount > 40) {
-            player.sendMessage(Text.literal("yep, that is in fact a tree").formatted(Formatting.GREEN), false);
-        } else {
-            player.sendMessage(Text.literal("nope, not a tree. no.").formatted(Formatting.RED), false);
-        }
-
-        if (this == AffinityItems.VICIOUS_WISP_MATTER) {
-            for (var pos : results) {
-                world.breakBlock(pos, true);
-            }
-        }
-
-        return ActionResult.SUCCESS;
+        return ActionResult.PASS;
     }
 
     @Override
