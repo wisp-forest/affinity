8f55d89f9e698ccf0f52fb7d1ee16950841baa7a
Basique Evangelist <basiqueevangelist@yandex.ru>
2022-10-31 01:15:09 +0300

    unholy water

---
+++ src/main/java/io/wispforest/affinity/mixin/DamageEnchantmentMixin.java
@@ -0,0 +1,29 @@
+package io.wispforest.affinity.mixin;
+
+import io.wispforest.affinity.enchantment.template.AffinityDamageEnchantment;
+import io.wispforest.affinity.object.AffinityStatusEffects;
+import net.minecraft.enchantment.DamageEnchantment;
+import net.minecraft.entity.EntityGroup;
+import net.minecraft.entity.LivingEntity;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+
+@Mixin(DamageEnchantment.class)
+public class DamageEnchantmentMixin implements AffinityDamageEnchantment {
+
+    @Shadow @Final public int typeIndex;
+
+    @Override
+    public boolean shouldApplyDamage(int level, LivingEntity attacker, LivingEntity target, float incomingDamage) {
+        if (typeIndex != 1) return false;
+        if (attacker.getGroup() == EntityGroup.UNDEAD) return false;
+
+        return attacker.hasStatusEffect(AffinityStatusEffects.UNHOLY);
+    }
+
+    @Override
+    public float getExtraDamage(int level, LivingEntity attacker, LivingEntity target, float incomingDamage) {
+        return 2.5f * level * (1 + attacker.getStatusEffect(AffinityStatusEffects.UNHOLY).getAmplifier());
+    }
+}
