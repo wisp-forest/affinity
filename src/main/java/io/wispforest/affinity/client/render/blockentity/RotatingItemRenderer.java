1f8cf67e96cb3b2a190f4632293e05017e02ee9f
glisco <glisco03@web.de>
2022-12-16 01:01:41 +0100

    add optional zRotation to RotatingItemRenderer

--- src/main/java/io/wispforest/affinity/client/render/blockentity/RotatingItemRenderer.java
+++ src/main/java/io/wispforest/affinity/client/render/blockentity/RotatingItemRenderer.java
@@ -10,6 +10,10 @@ import net.minecraft.util.math.RotationAxis;
 public interface RotatingItemRenderer {
 
     default void renderItem(MatrixStack matrices, VertexConsumerProvider consumers, ItemStack stack, double speedDivisor, float scale, float x, float y, float z, int light, int overlay) {
+        this.renderItem(matrices, consumers, stack, speedDivisor, scale, x, y, z, 0, light, overlay);
+    }
+
+    default void renderItem(MatrixStack matrices, VertexConsumerProvider consumers, ItemStack stack, double speedDivisor, float scale, float x, float y, float z, float zRotation, int light, int overlay) {
         matrices.push();
 
         final var client = MinecraftClient.getInstance();
@@ -18,7 +22,13 @@ public interface RotatingItemRenderer {
 
         matrices.translate(x, depthModel ? y - .05 : y, z);
         matrices.scale(scale, scale, scale);
+
         matrices.multiply(RotationAxis.POSITIVE_Y.rotation((float) ((System.currentTimeMillis() / speedDivisor) % (2 * Math.PI))));
+
+        matrices.translate(0, .125, 0);
+        matrices.multiply(RotationAxis.POSITIVE_Z.rotationDegrees(zRotation));
+        matrices.translate(0, -.125, 0);
+
         client.getItemRenderer().renderItem(stack, ModelTransformation.Mode.GROUND, light, overlay, matrices, consumers, 0);
 
         matrices.pop();
