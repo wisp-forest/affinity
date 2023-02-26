0641b0b0157768b631667364189d5c91633f42c3
glisco <glisco03@web.de>
2023-02-26 01:54:51 +0100

    track time in affine candles to add random offset and allow acceleration

--- src/main/java/io/wispforest/affinity/blockentity/impl/AffineCandleBlockEntity.java
+++ src/main/java/io/wispforest/affinity/blockentity/impl/AffineCandleBlockEntity.java
@@ -1,18 +1,21 @@
 package io.wispforest.affinity.blockentity.impl;
 
+import io.wispforest.affinity.block.impl.AffineCandleBlock;
 import io.wispforest.affinity.blockentity.template.AethumNetworkMemberBlockEntity;
 import io.wispforest.affinity.blockentity.template.TickedBlockEntity;
 import io.wispforest.affinity.component.AffinityComponents;
 import io.wispforest.affinity.object.AffinityBlocks;
 import net.minecraft.block.BlockState;
-import net.minecraft.block.CandleBlock;
-import net.minecraft.state.property.Properties;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
+
+import java.util.concurrent.ThreadLocalRandom;
 
 public class AffineCandleBlockEntity extends AethumNetworkMemberBlockEntity implements TickedBlockEntity {
 
     private static final Vec3d LINK_ATTACHMENT_POINT = new Vec3d(0, -.35f, 0);
+
+    private int time = ThreadLocalRandom.current().nextInt(100);
 
     public AffineCandleBlockEntity(BlockPos pos, BlockState state) {
         super(AffinityBlocks.Entities.AFFINE_CANDLE, pos, state);
@@ -23,21 +26,20 @@ public class AffineCandleBlockEntity extends AethumNetworkMemberBlockEntity impl
 
     @Override
     public void tickServer() {
-        if (!this.getCachedState().get(Properties.LIT)) return;
+        this.time++;
+        if (!this.getCachedState().get(AffineCandleBlock.LIT)) return;
 
-        long flux = this.fluxStorage.flux();
+        long flux = this.flux();
         if (flux > 0) this.updateFlux(flux - 1);
 
-        if (this.getCachedState().get(CandleBlock.CANDLES) < 3) return;
-        if (this.world.getTime() % 80 != 0) return;
+        if (this.getCachedState().get(AffineCandleBlock.CANDLES) < 3) return;
+        if (this.time % 80 != 0) return;
         if (flux <= 50) return;
 
-        var component = AffinityComponents.CHUNK_AETHUM.get(this.world.getChunk(this.pos));
+        final var component = AffinityComponents.CHUNK_AETHUM.get(this.world.getChunk(this.pos));
+        if (component.getAethum() >= 99.9) return;
 
-        // TODO: this really needs to be enforced by the chunk itself
-        if (component.getAethum() >= 100) return;
-        component.setAethum(component.getAethum() + 0.1);
-
+        component.addAethum(0.1);
         this.updateFlux(flux - 50);
     }
 
