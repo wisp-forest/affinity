9988ad5a6cd198220633a612004aa901d4feb367
glisco <glisco03@web.de>
2023-02-26 01:54:17 +0100

    remove old player aethum command nodes, merge 'aethumflux' command as 'flux' subcommand into main 'aethum' command

--- src/main/java/io/wispforest/affinity/misc/AffinityDebugCommands.java
+++ src/main/java/io/wispforest/affinity/misc/AffinityDebugCommands.java
@@ -3,13 +3,10 @@ package io.wispforest.affinity.misc;
 import com.mojang.brigadier.arguments.DoubleArgumentType;
 import com.mojang.brigadier.arguments.IntegerArgumentType;
 import com.mojang.brigadier.arguments.LongArgumentType;
-import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import io.wispforest.affinity.aethumflux.storage.AethumFluxContainer;
-import io.wispforest.affinity.component.AethumComponent;
 import io.wispforest.affinity.component.AffinityComponents;
-import io.wispforest.affinity.component.PlayerAethumComponent;
 import io.wispforest.owo.ops.TextOps;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.fabricmc.loader.api.FabricLoader;
@@ -26,8 +23,6 @@ import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.util.function.BiConsumer;
-import java.util.function.Function;
 
 import static net.minecraft.server.command.CommandManager.argument;
 import static net.minecraft.server.command.CommandManager.literal;
@@ -37,7 +32,6 @@ public class AffinityDebugCommands {
     public static final int AFFINITY_COLOR = 0x94B3FD;
     public static final int VALUE_COLOR = 0x2FDD92;
 
-    // TODO clean up player aethum nodes
     public static void register() {
         CommandRegistrationCallback.EVENT.register((dispatcher, access, environment) -> {
             dispatcher.register(literal("aethum")
@@ -57,20 +51,14 @@ public class AffinityDebugCommands {
                                     .then(literal("regenerate").executes(AffinityDebugCommands::regenerateChunkAethum))))
                     .then(literal("player")
                             .then(argument("player", EntityArgumentType.player())
-                                    .then(literal("get")
-//                                            .then(getPlayerAethumNode(PlayerAethumType.VALUE))
-//                                            .then(getPlayerAethumNode(PlayerAethumType.MAX))
-                                            .then(getPlayerAethumNode(PlayerAethumType.VALUE)))
+                                    .then(literal("get").executes(AffinityDebugCommands::getPlayerAethum))
                                     .then(literal("set")
-//                                            .then(setPlayerAethumNode(PlayerAethumType.VALUE))
-//                                            .then(setPlayerAethumNode(PlayerAethumType.MAX))
-                                            .then(setPlayerAethumNode(PlayerAethumType.VALUE))))));
-
-            dispatcher.register(literal("aethumflux")
-                    .then(argument("position", BlockPosArgumentType.blockPos())
-                            .then(literal("get").executes(AffinityDebugCommands::getAethumFluxAt))
-                            .then(literal("set")
-                                    .then(argument("flux", LongArgumentType.longArg(0)).executes(AffinityDebugCommands::setAethumFluxAt)))));
+                                            .then(argument("aethum", DoubleArgumentType.doubleArg()).executes(AffinityDebugCommands::setPlayerAethum)))))
+                    .then(literal("flux")
+                            .then(argument("position", BlockPosArgumentType.blockPos())
+                                    .then(literal("get").executes(AffinityDebugCommands::getAethumFluxAt))
+                                    .then(literal("set")
+                                            .then(argument("flux", LongArgumentType.longArg(0)).executes(AffinityDebugCommands::setAethumFluxAt))))));
         });
     }
 
@@ -100,16 +88,6 @@ public class AffinityDebugCommands {
         }
 
         return 0;
-    }
-
-    private static LiteralArgumentBuilder<ServerCommandSource> setPlayerAethumNode(PlayerAethumType type) {
-        return literal(type.name().toLowerCase()).then(argument("aethum", DoubleArgumentType.doubleArg())
-                .executes(context -> setPlayerAethum(context, type)));
-    }
-
-    private static LiteralArgumentBuilder<ServerCommandSource> getPlayerAethumNode(PlayerAethumType type) {
-        return literal(type.name().toLowerCase())
-                .executes(context -> getPlayerAethum(context, type));
     }
 
     private static int regenerateChunkAethum(CommandContext<ServerCommandSource> context) throws CommandSyntaxException {
@@ -194,18 +172,18 @@ public class AffinityDebugCommands {
         return 0;
     }
 
-    private static int getPlayerAethum(CommandContext<ServerCommandSource> context, PlayerAethumType type) throws CommandSyntaxException {
-        final double playerAethum = type.getter.apply(AffinityComponents.PLAYER_AETHUM.get(context.getSource().getPlayer()));
-        context.getSource().sendFeedback(valueFeedback("player " + type.displayName, playerAethum), true);
+    private static int getPlayerAethum(CommandContext<ServerCommandSource> context) {
+        final double playerAethum = AffinityComponents.PLAYER_AETHUM.get(context.getSource().getPlayer()).getAethum();
+        context.getSource().sendFeedback(valueFeedback("player aethum", playerAethum), true);
 
         return (int) Math.round(playerAethum);
     }
 
-    private static int setPlayerAethum(CommandContext<ServerCommandSource> context, PlayerAethumType type) throws CommandSyntaxException {
+    private static int setPlayerAethum(CommandContext<ServerCommandSource> context) throws CommandSyntaxException {
         final var aethum = DoubleArgumentType.getDouble(context, "aethum");
-        type.setter.accept(AffinityComponents.PLAYER_AETHUM.get(EntityArgumentType.getPlayer(context, "player")), aethum);
+        AffinityComponents.PLAYER_AETHUM.get(EntityArgumentType.getPlayer(context, "player")).setAethum(aethum);
 
-        context.getSource().sendFeedback(simpleFeedback("player " + type.displayName + " updated"), true);
+        context.getSource().sendFeedback(simpleFeedback("player aethum updated"), true);
 
         return (int) Math.round(aethum);
     }
@@ -216,22 +194,6 @@ public class AffinityDebugCommands {
 
     private static Text valueFeedback(String message, Object value) {
         return TextOps.withColor("affinity §> " + message + ": §" + value, AFFINITY_COLOR, TextOps.color(Formatting.GRAY), VALUE_COLOR);
-    }
-
-    private enum PlayerAethumType {
-        VALUE("aethum", PlayerAethumComponent::getAethum, AethumComponent::setAethum);
-//        MAX("max aethum", PlayerAethumComponent::getMaxAethum, PlayerAethumComponent::setMaxAethum),
-//        REGEN("aethum regen speed", PlayerAethumComponent::getNaturalRegenSpeed, PlayerAethumComponent::setNaturalRegenSpeed);
-
-        public final String displayName;
-        public final Function<PlayerAethumComponent, Double> getter;
-        public final BiConsumer<PlayerAethumComponent, Double> setter;
-
-        PlayerAethumType(String displayName, Function<PlayerAethumComponent, Double> getter, BiConsumer<PlayerAethumComponent, Double> setter) {
-            this.displayName = displayName;
-            this.getter = getter;
-            this.setter = setter;
-        }
     }
 
 }
