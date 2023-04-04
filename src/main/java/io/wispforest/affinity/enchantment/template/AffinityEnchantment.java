bb183e788316c48e9b44e7bb1bf368854f6288d7
glisco <glisco03@web.de>
2023-04-04 19:04:43 +0200

    remove all affinity enchantments from random selection

--- src/main/java/io/wispforest/affinity/enchantment/template/AffinityEnchantment.java
+++ src/main/java/io/wispforest/affinity/enchantment/template/AffinityEnchantment.java
@@ -5,14 +5,19 @@ import net.minecraft.enchantment.EnchantmentTarget;
 import net.minecraft.entity.EquipmentSlot;
 
 import java.util.Arrays;
-import java.util.List;
+import java.util.EnumSet;
 
 public abstract class AffinityEnchantment extends Enchantment {
 
-    protected final List<EquipmentSlot> slotTypes;
+    protected final EnumSet<EquipmentSlot> slotTypes;
 
     protected AffinityEnchantment(Rarity weight, EnchantmentTarget type, EquipmentSlot... slotTypes) {
         super(weight, type, slotTypes);
-        this.slotTypes = Arrays.asList(slotTypes);
+        this.slotTypes = EnumSet.copyOf(Arrays.asList(slotTypes));
+    }
+
+    @Override
+    public boolean isAvailableForRandomSelection() {
+        return false;
     }
 }
