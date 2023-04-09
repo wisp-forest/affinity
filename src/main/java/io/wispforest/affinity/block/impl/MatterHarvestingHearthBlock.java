8b84945a14dc5382b9db7107ae2d46133262e134
glisco <glisco03@web.de>
2023-04-09 02:00:32 +0200

    add horizontal axis property to matter harvesting hearth

--- src/main/java/io/wispforest/affinity/block/impl/MatterHarvestingHearthBlock.java
+++ src/main/java/io/wispforest/affinity/block/impl/MatterHarvestingHearthBlock.java
@@ -16,13 +16,16 @@ import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.BlockEntityTicker;
 import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.BooleanProperty;
+import net.minecraft.state.property.EnumProperty;
 import net.minecraft.state.property.Properties;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.random.Random;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
@@ -35,7 +38,19 @@ import java.util.stream.Stream;
 public class MatterHarvestingHearthBlock extends AethumNetworkMemberBlock {
 
     public static final BooleanProperty LIT = Properties.LIT;
-    private static final VoxelShape SHAPE = Stream.of(
+    public static final EnumProperty<Direction.Axis> AXIS = Properties.HORIZONTAL_AXIS;
+
+    private static final VoxelShape X_SHAPE = Stream.of(
+            Block.createCuboidShape(0, 0, 0, 16, 4, 4),
+            Block.createCuboidShape(6, 4, 1, 10, 5, 3),
+            Block.createCuboidShape(6, 4, 13, 10, 5, 15),
+            Block.createCuboidShape(0, 0, 12, 16, 4, 16),
+            Block.createCuboidShape(1, 0, 4, 4, 4, 12),
+            Block.createCuboidShape(12, 0, 4, 15, 4, 12),
+            Block.createCuboidShape(4, 1, 4, 12, 3, 12)
+    ).reduce(VoxelShapes::union).get();
+
+    private static final VoxelShape Z_SHAPE = Stream.of(
             Block.createCuboidShape(0, 0, 0, 4, 4, 16),
             Block.createCuboidShape(1, 4, 6, 3, 5, 10),
             Block.createCuboidShape(13, 4, 6, 15, 5, 10),
@@ -47,17 +62,23 @@ public class MatterHarvestingHearthBlock extends AethumNetworkMemberBlock {
 
     public MatterHarvestingHearthBlock() {
         super(FabricBlockSettings.copyOf(Blocks.CAMPFIRE));
-        this.setDefaultState(this.getDefaultState().with(LIT, false));
+        this.setDefaultState(this.getDefaultState().with(LIT, false).with(AXIS, Direction.Axis.X));
     }
 
     @Override
     protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
-        builder.add(LIT);
+        builder.add(LIT, AXIS);
+    }
+
+    @Nullable
+    @Override
+    public BlockState getPlacementState(ItemPlacementContext ctx) {
+        return this.getDefaultState().with(AXIS, ctx.getHorizontalPlayerFacing().getAxis());
     }
 
     @Override
     public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
-        return SHAPE;
+        return state.get(AXIS) == Direction.Axis.X ? X_SHAPE : Z_SHAPE;
     }
 
     @Override
