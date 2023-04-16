4d1172a6e28a12b5f7ec94b639c1d1606c6ed3ef
glisco <glisco03@web.de>
2023-04-16 04:09:52 +0200

    display "regeneration" text in bastion regen duration in standard white

--- src/main/java/io/wispforest/affinity/mixin/StatusEffectUtilMixin.java
+++ src/main/java/io/wispforest/affinity/mixin/StatusEffectUtilMixin.java
@@ -5,6 +5,7 @@ import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffectUtil;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -16,7 +17,7 @@ public class StatusEffectUtilMixin {
     @Inject(method = "durationToString", at = @At("HEAD"), cancellable = true)
     private static void injectBastionRegenerationName(StatusEffectInstance effect, float multiplier, CallbackInfoReturnable<Text> cir) {
         if (effect.getEffectType() != AffinityStatusEffects.BASTION_REGENERATION) return;
-        cir.setReturnValue(StatusEffects.REGENERATION.getName());
+        cir.setReturnValue(StatusEffects.REGENERATION.getName().copy().formatted(Formatting.WHITE));
     }
 
 }
