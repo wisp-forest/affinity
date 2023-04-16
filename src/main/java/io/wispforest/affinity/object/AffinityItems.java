44cfc874db10f3345afb1810c73c44f56d34a597
glisco <glisco03@web.de>
2023-04-17 01:46:54 +0200

    ring improvements  - improve featherweight ring texture  - add archetypal iron ring as base for all rings  - add recipes for both rings

--- src/main/java/io/wispforest/affinity/object/AffinityItems.java
+++ src/main/java/io/wispforest/affinity/object/AffinityItems.java
@@ -72,6 +72,7 @@ public class AffinityItems implements ItemRegistryContainer {
     public static final Item RESONANCE_CRYSTAL = new GlintItem(settings(AffinityItemGroup.MAIN).rarity(Rarity.UNCOMMON));
     public static final Item SYNTHETIC_DRAGON_HEART = new GlintItem(settings(AffinityItemGroup.MAIN).rarity(Rarity.RARE));
     public static final Item AETHUM_OVERCHARGER = new GlintItem(settings(AffinityItemGroup.MAIN).rarity(Rarity.RARE).maxCount(1));
+    public static final Item ARCHETYPAL_IRON_RING = new Item(settings(AffinityItemGroup.MAIN));
     public static final Item FEATHERWEIGHT_RING = new FeatherweightRingItem();
 
     public static final Item ARCANE_FADE_BUCKET = new BucketItem(AffinityBlocks.Fluids.ARCANE_FADE, settings(AffinityItemGroup.MAIN).maxCount(1).recipeRemainder(Items.BUCKET));
