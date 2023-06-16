788b228c68a43b69162d27b8fd1fa9a546d9bdd3
glisco <glisco03@web.de>
2023-06-17 01:03:33 +0200

    implement crystallized experience

--- src/main/java/io/wispforest/affinity/client/CarbonCopyTooltipComponent.java
+++ src/main/java/io/wispforest/affinity/client/CarbonCopyTooltipComponent.java
@@ -2,6 +2,7 @@ package io.wispforest.affinity.client;
 
 import io.wispforest.affinity.Affinity;
 import io.wispforest.affinity.item.CarbonCopyItem;
+import io.wispforest.owo.ui.base.BaseOwoTooltipComponent;
 import io.wispforest.owo.ui.component.Components;
 import io.wispforest.owo.ui.container.Containers;
 import io.wispforest.owo.ui.container.StackLayout;
@@ -13,66 +14,44 @@ import net.minecraft.client.render.item.ItemRenderer;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.item.ItemStack;
 
-public class CarbonCopyTooltipComponent implements TooltipComponent {
-
-    private final StackLayout root;
+@SuppressWarnings("UnstableApiUsage")
+public class CarbonCopyTooltipComponent extends BaseOwoTooltipComponent<StackLayout> {
 
     public CarbonCopyTooltipComponent(CarbonCopyItem.TooltipData data) {
-        this.root = Containers.stack(Sizing.content(), Sizing.content());
-        this.root.horizontalAlignment(HorizontalAlignment.LEFT).verticalAlignment(VerticalAlignment.CENTER);
+        super(() -> {
+            var root = Containers.stack(Sizing.content(), Sizing.content());
+            root.horizontalAlignment(HorizontalAlignment.LEFT).verticalAlignment(VerticalAlignment.CENTER);
 
-        this.root.child(Components.texture(Affinity.id("textures/gui/carbon_copy_tooltip.png"), 0, 0, 128, 64, 128, 64));
+            root.child(Components.texture(Affinity.id("textures/gui/carbon_copy_tooltip.png"), 0, 0, 128, 64, 128, 64));
 
-        int height, width = 1;
-        outer:
-        for (height = 1; height <= 3; height++) {
-            for (width = 1; width <= 3; width++) {
-                if (!data.recipe().fits(width, height)) continue;
-                break outer;
-            }
-        }
-
-        var grid = Containers.grid(Sizing.content(), Sizing.content(), 3, 3);
-        for (int y = 0; y < 3; y++) {
-            for (int x = 0; x < 3; x++) {
-                var displayStack = ItemStack.EMPTY;
-                if (x < width && y < height) {
-                    var displayStacks = data.recipe().getIngredients().get(y * width + x).getMatchingStacks();
-                    if (displayStacks.length > 0) {
-                        displayStack = displayStacks[(int) (System.currentTimeMillis() / 1000 % displayStacks.length)];
-                    }
+            int height, width = 1;
+            outer:
+            for (height = 1; height <= 3; height++) {
+                for (width = 1; width <= 3; width++) {
+                    if (!data.recipe().fits(width, height)) continue;
+                    break outer;
                 }
-
-                grid.child(Components.item(displayStack).showOverlay(true).margins(Insets.of(1)), y, x);
             }
-        }
 
-        this.root.child(grid.margins(Insets.left(5)));
-        this.root.child(Components.item(data.result()).showOverlay(true).positioning(Positioning.absolute(106, 23)));
+            var grid = Containers.grid(Sizing.content(), Sizing.content(), 3, 3);
+            for (int y = 0; y < 3; y++) {
+                for (int x = 0; x < 3; x++) {
+                    var displayStack = ItemStack.EMPTY;
+                    if (x < width && y < height) {
+                        var displayStacks = data.recipe().getIngredients().get(y * width + x).getMatchingStacks();
+                        if (displayStacks.length > 0) {
+                            displayStack = displayStacks[(int) (System.currentTimeMillis() / 1000 % displayStacks.length)];
+                        }
+                    }
 
-        this.root.inflate(Size.of(1000, 1000));
-        this.root.mount(null, 0, 0);
-    }
+                    grid.child(Components.item(displayStack).showOverlay(true).margins(Insets.of(1)), y, x);
+                }
+            }
 
-    @Override
-    public void drawItems(TextRenderer textRenderer, int x, int y, DrawContext context) {
-        context = OwoUIDrawContext.of(context);
-        context.getMatrices().push();
-        context.getMatrices().translate(0, 0, 1000);
+            root.child(grid.margins(Insets.left(5)));
+            root.child(Components.item(data.result()).showOverlay(true).positioning(Positioning.absolute(106, 23)));
 
-        this.root.moveTo(x, y);
-        this.root.draw((OwoUIDrawContext) context, 0, 0, 0, 0);
-
-        context.getMatrices().pop();
-    }
-
-    @Override
-    public int getHeight() {
-        return this.root.height();
-    }
-
-    @Override
-    public int getWidth(TextRenderer textRenderer) {
-        return this.root.width();
+            return root;
+        });
     }
 }
