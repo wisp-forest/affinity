5f0f005ec47d50f6f51bd7031a460a18ba2ab09e
glisco <glisco03@web.de>
2023-07-23 23:23:22 +0200

    fix item group on servers

--- src/main/java/io/wispforest/affinity/item/AffinityItemGroup.java
+++ src/main/java/io/wispforest/affinity/item/AffinityItemGroup.java
@@ -7,6 +7,8 @@ import io.wispforest.affinity.object.AffinityItems;
 import io.wispforest.owo.itemgroup.Icon;
 import io.wispforest.owo.itemgroup.OwoItemGroup;
 import io.wispforest.owo.itemgroup.gui.ItemGroupButton;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.enchantment.EnchantmentLevelEntry;
 import net.minecraft.item.EnchantedBookItem;
@@ -27,20 +29,8 @@ public class AffinityItemGroup {
     private static OwoItemGroup GROUP;
     public static void register() {
         GROUP = OwoItemGroup.builder(Affinity.id("affinity"), () -> Icon.of(AffinityItems.INERT_WISP_MATTER)).initializer(group -> {
-            group.addTab(Icon.of(AffinityItems.EMERALD_WAND_OF_IRIDESCENCE), "main", null, true);
-            group.addTab(Icon.of(AffinityBlocks.AZALEA_LOG), "nature", null, false);
-            group.addTab(Icon.of(AffinityItems.RESOUNDING_CHIME), "equipment", null, false);
-            group.addCustomTab(Icon.of(AffinityItems.RESPLENDENT_GEM), "enchantments", (context, entries) -> {
-                Registries.ENCHANTMENT.getIds().stream()
-                        .filter(id -> id.getNamespace().equals(Affinity.MOD_ID))
-                        .map(Registries.ENCHANTMENT::get)
-                        .filter(enchantment -> !(enchantment instanceof AbsoluteEnchantment))
-                        .map(enchantment -> new EnchantmentLevelEntry(enchantment, enchantment.getMaxLevel()))
-                        .map(EnchantedBookItem::forEnchantment)
-                        .forEach(entries::add);
-            }, false);
-
-            group.addButton(ItemGroupButton.github(group, "https://github.com/wisp-forest/affinity"));
+            //noinspection Convert2MethodRef
+            initializeGroup(group);
         }).build();
 
         ItemGroupEvents.modifyEntriesEvent(RegistryKey.of(RegistryKeys.ITEM_GROUP, GROUP.id())).register(entries -> {
@@ -51,5 +41,23 @@ public class AffinityItemGroup {
 
     public static OwoItemGroup group() {
         return Objects.requireNonNull(GROUP, "Affinity item group not initialized");
+    }
+
+    @Environment(EnvType.CLIENT)
+    private static void initializeGroup(OwoItemGroup group) {
+        group.addTab(Icon.of(AffinityItems.EMERALD_WAND_OF_IRIDESCENCE), "main", null, true);
+        group.addTab(Icon.of(AffinityBlocks.AZALEA_LOG), "nature", null, false);
+        group.addTab(Icon.of(AffinityItems.RESOUNDING_CHIME), "equipment", null, false);
+        group.addCustomTab(Icon.of(AffinityItems.RESPLENDENT_GEM), "enchantments", (context, entries) -> {
+            Registries.ENCHANTMENT.getIds().stream()
+                    .filter(id -> id.getNamespace().equals(Affinity.MOD_ID))
+                    .map(Registries.ENCHANTMENT::get)
+                    .filter(enchantment -> !(enchantment instanceof AbsoluteEnchantment))
+                    .map(enchantment -> new EnchantmentLevelEntry(enchantment, enchantment.getMaxLevel()))
+                    .map(EnchantedBookItem::forEnchantment)
+                    .forEach(entries::add);
+        }, false);
+
+        group.addButton(ItemGroupButton.github(group, "https://github.com/wisp-forest/affinity"));
     }
 }
