d9b0c5006729a1690ed30286e8f270763e782947
glisco <glisco03@web.de>
2023-09-12 01:01:42 +0200

    move item renderer entity context capture to HEAD to avoid conflict with interactic

--- src/main/java/io/wispforest/affinity/mixin/client/ItemEntityRendererMixin.java
+++ src/main/java/io/wispforest/affinity/mixin/client/ItemEntityRendererMixin.java
@@ -11,10 +11,10 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(ItemEntityRenderer.class)
+@Mixin(value = ItemEntityRenderer.class, priority = 900)
 public class ItemEntityRendererMixin {
 
-    @Inject(method = "render(Lnet/minecraft/entity/ItemEntity;FFLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;I)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/item/ItemRenderer;renderItem(Lnet/minecraft/item/ItemStack;Lnet/minecraft/client/render/model/json/ModelTransformationMode;ZLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;IILnet/minecraft/client/render/model/BakedModel;)V"))
+    @Inject(method = "render(Lnet/minecraft/entity/ItemEntity;FFLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;I)V", at = @At("HEAD"))
     private void captureRenderItem(ItemEntity itemEntity, float f, float g, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i, CallbackInfo ci) {
         MixinHooks.renderItem = EntityReference.of(itemEntity);
     }
