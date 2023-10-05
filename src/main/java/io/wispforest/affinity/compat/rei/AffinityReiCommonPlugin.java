30fb3c011398eaae091bf6e230a07e2669ac9e7c
glisco <glisco03@web.de>
2023-10-05 18:26:31 +0200

    stop voiding items when autofilling recipes in assembly augment through REI

--- src/main/java/io/wispforest/affinity/compat/rei/AffinityReiCommonPlugin.java
+++ src/main/java/io/wispforest/affinity/compat/rei/AffinityReiCommonPlugin.java
@@ -8,11 +8,14 @@ import me.shedaniel.rei.api.common.display.DisplaySerializerRegistry;
 import me.shedaniel.rei.api.common.entry.type.EntryType;
 import me.shedaniel.rei.api.common.entry.type.EntryTypeRegistry;
 import me.shedaniel.rei.api.common.plugins.REIServerPlugin;
+import me.shedaniel.rei.api.common.transfer.info.MenuInfoContext;
 import me.shedaniel.rei.api.common.transfer.info.MenuInfoRegistry;
 import me.shedaniel.rei.api.common.transfer.info.simple.RecipeBookGridMenuInfo;
 import me.shedaniel.rei.api.common.transfer.info.simple.SimpleMenuInfoProvider;
 import me.shedaniel.rei.plugin.common.displays.crafting.DefaultCraftingDisplay;
 import net.minecraft.entity.effect.StatusEffect;
+
+import java.util.stream.IntStream;
 
 public class AffinityReiCommonPlugin implements REIServerPlugin {
 
@@ -29,7 +32,7 @@ public class AffinityReiCommonPlugin implements REIServerPlugin {
 
     @Override
     public void registerMenuInfo(MenuInfoRegistry registry) {
-        registry.register(ASSEMBLY, AssemblyAugmentScreenHandler.class, SimpleMenuInfoProvider.of(RecipeBookGridMenuInfo::new));
+        registry.register(ASSEMBLY, AssemblyAugmentScreenHandler.class, SimpleMenuInfoProvider.of(AssemblyAugmentMenuInfo::new));
     }
 
     @Override
@@ -40,5 +43,17 @@ public class AffinityReiCommonPlugin implements REIServerPlugin {
     @Override
     public void registerDisplaySerializer(DisplaySerializerRegistry registry) {
         registry.register(ASSEMBLY, DefaultCraftingDisplay.serializer());
+    }
+
+    public static class AssemblyAugmentMenuInfo extends RecipeBookGridMenuInfo<AssemblyAugmentScreenHandler, DefaultCraftingDisplay<?>> {
+
+        public AssemblyAugmentMenuInfo(DefaultCraftingDisplay<?> display) {
+            super(display);
+        }
+
+        @Override
+        public IntStream getInputStackSlotIds(MenuInfoContext<AssemblyAugmentScreenHandler, ?, DefaultCraftingDisplay<?>> context) {
+            return IntStream.concat(super.getInputStackSlotIds(context), IntStream.of(context.getMenu().slots.get(context.getMenu().slots.size() - 1).id));
+        }
     }
 }
