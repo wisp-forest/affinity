35076136508ad1d97eea568a7ecb68d9b6874d00
glisco <glisco03@web.de>
2023-10-05 18:40:19 +0200

    replace forced block entity rendering with more aggressive implementation to sidestep incompatibility with entity culling

--- src/main/java/io/wispforest/affinity/mixin/client/BlockEntityRenderDispatcherMixin.java
+++ src/main/java/io/wispforest/affinity/mixin/client/BlockEntityRenderDispatcherMixin.java
@@ -2,19 +2,37 @@ package io.wispforest.affinity.mixin.client;
 
 import io.wispforest.affinity.misc.MixinHooks;
 import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.block.entity.BlockEntityRenderDispatcher;
-import net.minecraft.util.math.Vec3d;
+import net.minecraft.client.render.block.entity.BlockEntityRenderer;
+import net.minecraft.client.util.math.MatrixStack;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(BlockEntityRenderDispatcher.class)
-public class BlockEntityRenderDispatcherMixin {
+public abstract class BlockEntityRenderDispatcherMixin<E extends BlockEntity> {
 
-    @ModifyArg(method = "render(Lnet/minecraft/block/entity/BlockEntity;FLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/block/entity/BlockEntityRenderer;isInRenderDistance(Lnet/minecraft/block/entity/BlockEntity;Lnet/minecraft/util/math/Vec3d;)Z"))
-    private Vec3d fakeCameraPosition(BlockEntity entity, Vec3d cameraPosIn) {
-        if (!MixinHooks.forceBlockEntityRendering) return cameraPosIn;
-        return Vec3d.ofCenter(entity.getPos());
+    @Shadow
+    private static void runReported(BlockEntity blockEntity, Runnable runnable) {}
+
+    @Shadow
+    private static <T extends BlockEntity> void render(BlockEntityRenderer<T> renderer, T blockEntity, float tickDelta, MatrixStack matrices, VertexConsumerProvider vertexConsumers) {}
+
+    // I majorly dislike this way of doing it, but without pulling
+    // in MixinExtras (which seems a bit excessive for a single inject) I don't
+    // think there's much else that can be done
+    //
+    // glisco, 19.09.2023
+    @Inject(method = "render(Lnet/minecraft/block/entity/BlockEntity;FLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/block/entity/BlockEntityRenderer;isInRenderDistance(Lnet/minecraft/block/entity/BlockEntity;Lnet/minecraft/util/math/Vec3d;)Z"), locals = LocalCapture.CAPTURE_FAILHARD, cancellable = true)
+    private void forceRendering(E blockEntity, float tickDelta, MatrixStack matrices, VertexConsumerProvider vertexConsumers, CallbackInfo ci, BlockEntityRenderer<E> blockEntityRenderer) {
+        if (!MixinHooks.forceBlockEntityRendering) return;
+
+        runReported(blockEntity, () -> render(blockEntityRenderer, blockEntity, tickDelta, matrices, vertexConsumers));
+        ci.cancel();
     }
 
 }
