2e98effa5d00992b6eed27116847157e3f7910a8
glisco <glisco03@web.de>
2023-11-11 23:17:33 +0100

    emit block break world event when crop reaper activates, tweak texture

--- src/main/java/io/wispforest/affinity/blockentity/impl/CropReaperBlockEntity.java
+++ src/main/java/io/wispforest/affinity/blockentity/impl/CropReaperBlockEntity.java
@@ -5,9 +5,11 @@ import io.wispforest.affinity.blockentity.template.InquirableOutlineProvider;
 import io.wispforest.affinity.blockentity.template.TickedBlockEntity;
 import io.wispforest.affinity.client.render.CuboidRenderer;
 import io.wispforest.affinity.object.AffinityBlocks;
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.CropBlock;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.WorldEvents;
 import org.jetbrains.annotations.Nullable;
 
 public class CropReaperBlockEntity extends AethumNetworkMemberBlockEntity implements TickedBlockEntity, InquirableOutlineProvider {
@@ -32,7 +34,9 @@ public class CropReaperBlockEntity extends AethumNetworkMemberBlockEntity implem
             var state = this.world.getBlockState(pos);
             if (!(state.getBlock() instanceof CropBlock crop) || !crop.isMature(state)) continue;
 
-            this.world.setBlockState(pos, crop.withAge(0));
+            this.world.setBlockState(pos, crop.withAge(0), Block.NOTIFY_LISTENERS);
+            this.world.syncWorldEvent(WorldEvents.BLOCK_BROKEN, pos, Block.getRawIdFromState(state));
+
             addedAethum += 50;
         }
 
