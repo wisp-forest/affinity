349d6b689ae062bcc61e368856b3533d46d1e8b3
glisco <glisco03@web.de>
2024-05-13 00:52:15 +0200

    fix chyz's mess in StyleMixin

--- src/main/java/io/wispforest/affinity/mixin/client/StyleMixin.java
+++ src/main/java/io/wispforest/affinity/mixin/client/StyleMixin.java
@@ -1,19 +1,18 @@
 package io.wispforest.affinity.mixin.client;
 
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import io.wispforest.affinity.misc.MixinHooks;
 import net.minecraft.text.Style;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Style.class)
 public class StyleMixin {
 
-    @Inject(method = "isObfuscated", at = @At("HEAD"), cancellable = true)
-    private void weObfuscaten(CallbackInfoReturnable<Boolean> cir) {
-        if (!MixinHooks.textObfuscation) return;
-        cir.setReturnValue(!cir.getReturnValueZ());
+    @ModifyReturnValue(method = "isObfuscated", at = @At("TAIL"))
+    private boolean weObfuscaten(boolean original) {
+        if (!MixinHooks.textObfuscation) return original;
+        return !original;
     }
 
 }
