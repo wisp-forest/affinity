e93dfb0f276539fb214c1e3861231b6baadc8fb3
glisco <glisco03@web.de>
2024-08-15 13:03:05 +0200

    local displacement gateway changes - add to item group - add loot table and mineability tag - make targeting algorithm consider player hitbox height

--- src/main/java/io/wispforest/affinity/block/impl/LocalDisplacementGatewayBlock.java
+++ src/main/java/io/wispforest/affinity/block/impl/LocalDisplacementGatewayBlock.java
@@ -3,15 +3,11 @@ package io.wispforest.affinity.block.impl;
 import com.mojang.serialization.MapCodec;
 import io.wispforest.affinity.blockentity.impl.LocalDisplacementGatewayBlockEntity;
 import io.wispforest.affinity.component.AffinityComponents;
-import io.wispforest.affinity.object.AffinityBlocks;
 import io.wispforest.affinity.object.AffinityParticleSystems;
-import io.wispforest.owo.particles.ClientParticles;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.*;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.particle.ParticleTypes;
-import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
@@ -19,7 +15,6 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
-import net.minecraft.world.BlockStateRaycastContext;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 
@@ -49,10 +44,19 @@ public class LocalDisplacementGatewayBlock extends BlockWithEntity {
 
     @Override
     public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
-        var targetPos = pos.offset(hit.getSide().getOpposite());
-        if (hit.getSide() == Direction.UP) targetPos = targetPos.down();
+        var playerHeight = player.getHeight();
 
-        if (!world.isAir(targetPos) || !world.isAir(targetPos.up())) return ActionResult.PASS;
+        BlockPos targetPos;
+        if (hit.getSide() == Direction.UP) {
+            var basePos = pos.getY() - playerHeight;
+            targetPos = BlockPos.ofFloored(pos.getX(), basePos, pos.getZ());
+        } else {
+            targetPos = pos.offset(hit.getSide().getOpposite());
+        }
+
+        if (!world.isAir(targetPos) || !world.isAir(BlockPos.ofFloored(targetPos.getX(), targetPos.getY() + playerHeight, targetPos.getZ()))) {
+            return ActionResult.PASS;
+        }
 
         var aethum = player.getComponent(AffinityComponents.PLAYER_AETHUM);
         if (!aethum.tryConsumeAethum(1.5f)) return ActionResult.PASS;
