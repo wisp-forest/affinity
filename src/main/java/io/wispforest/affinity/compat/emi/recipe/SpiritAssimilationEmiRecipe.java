61af97322472bed5368f4585b0f4fcda70d28017
glisco <glisco03@web.de>
2024-10-23 22:59:26 +0200

    stop using cloth math 'Point' type in EMI recipes (fixes #27)

--- src/main/java/io/wispforest/affinity/compat/emi/recipe/SpiritAssimilationEmiRecipe.java
+++ src/main/java/io/wispforest/affinity/compat/emi/recipe/SpiritAssimilationEmiRecipe.java
@@ -5,7 +5,6 @@ import dev.emi.emi.api.stack.EmiIngredient;
 import dev.emi.emi.api.stack.EmiStack;
 import dev.emi.emi.api.widget.Bounds;
 import dev.emi.emi.api.widget.FillingArrowWidget;
-import dev.emi.emi.api.widget.SlotWidget;
 import dev.emi.emi.api.widget.WidgetHolder;
 import io.wispforest.affinity.compat.emi.AffinityEmiPlugin;
 import io.wispforest.affinity.compat.emi.EmiUIAdapter;
@@ -16,7 +15,6 @@ import io.wispforest.owo.ui.component.EntityComponent;
 import io.wispforest.owo.ui.container.Containers;
 import io.wispforest.owo.ui.container.FlowLayout;
 import io.wispforest.owo.ui.core.*;
-import me.shedaniel.math.Point;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.entity.EntityType;
 import net.minecraft.text.Text;
@@ -34,8 +32,8 @@ public class SpiritAssimilationEmiRecipe extends BasicEmiRecipe {
         this.recipe = recipe;
 
         this.inputs = Stream.concat(
-                this.recipe.coreInputs.stream(),
-                this.recipe.getIngredients().stream()
+            this.recipe.coreInputs.stream(),
+            this.recipe.getIngredients().stream()
         ).map(AffinityEmiPlugin::veryCoolFeatureYouGotThereEmi).toList();
         this.outputs = List.of(EmiStack.of(this.recipe.getResult(null)));
     }
@@ -54,7 +52,7 @@ public class SpiritAssimilationEmiRecipe extends BasicEmiRecipe {
         inputContainer.verticalAlignment(VerticalAlignment.CENTER).horizontalAlignment(HorizontalAlignment.CENTER).margins(Insets.right(3));
         root.child(inputContainer);
 
-        var center = new Point((bounds.height() - 8) / 2 - 9, (bounds.height() - 8) / 2 - 9);
+        int centerX = (bounds.height() - 8) / 2 - 9, centerY = (bounds.height() - 8) / 2 - 9;
         double angleStep = Math.PI / (this.getInputs().size() - 4) * 2;
 
         for (int i = 4; i < this.getInputs().size(); i++) {
@@ -63,7 +61,7 @@ public class SpiritAssimilationEmiRecipe extends BasicEmiRecipe {
 
             inputContainer.child(adapter.wrap((x, y) -> {
                 return AffinityEmiPlugin.slot(this.getInputs().get(index), x, y).drawBack(false);
-            }).positioning(Positioning.absolute((int) (center.x + Math.cos(angle) * 40), (int) (center.y + Math.sin(angle) * 40))));
+            }).positioning(Positioning.absolute((int) (centerX + Math.cos(angle) * 40), (int) (centerY + Math.sin(angle) * 40))));
         }
 
         var inputGrid = Containers.grid(Sizing.content(), Sizing.content(), 2, 2);
@@ -81,17 +79,17 @@ public class SpiritAssimilationEmiRecipe extends BasicEmiRecipe {
             container.gap(5).horizontalAlignment(HorizontalAlignment.CENTER);
 
             container.child((this.recipe.entityType == EntityType.PLAYER
-                            ? Components.entity(Sizing.fixed(30), EntityComponent.createRenderablePlayer(MinecraftClient.getInstance().player.getGameProfile()))
-                            : Components.entity(Sizing.fixed(30), this.recipe.entityType, this.recipe.entityNbt()))
-                            .scaleToFit(true).tooltip(this.recipe.entityType.getName()))
-                    .child(adapter.wrap((x, y) -> new FillingArrowWidget(x, y, this.recipe.duration * 50))
-                            .tooltip(Text.of(MathUtil.rounded(this.recipe.duration / 20d, 1) + "s")));
+                    ? Components.entity(Sizing.fixed(30), EntityComponent.createRenderablePlayer(MinecraftClient.getInstance().player.getGameProfile()))
+                    : Components.entity(Sizing.fixed(30), this.recipe.entityType, this.recipe.entityNbt()))
+                    .scaleToFit(true).tooltip(this.recipe.entityType.getName()))
+                .child(adapter.wrap((x, y) -> new FillingArrowWidget(x, y, this.recipe.duration * 50))
+                    .tooltip(Text.of(MathUtil.rounded(this.recipe.duration / 20d, 1) + "s")));
 
             if (this.recipe.fluxCostPerTick != 0) {
                 container.child(Components.label(Text.of(this.recipe.fluxCostPerTick * this.recipe.duration + "\n" + "flux"))
-                        .horizontalTextAlignment(HorizontalAlignment.CENTER)
-                        .color(Color.ofRgb(0x3f3f3f))
-                        .margins(Insets.top(10)));
+                    .horizontalTextAlignment(HorizontalAlignment.CENTER)
+                    .color(Color.ofRgb(0x3f3f3f))
+                    .margins(Insets.top(10)));
             } else {
                 container.padding(Insets.bottom(35));
             }
